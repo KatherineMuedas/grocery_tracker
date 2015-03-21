df245d444997f1fbff765a95bde7a9759d05b24b
katherine <muedasnka@gmail.com>
2015-03-20 21:26:34 -0600

    fixs bug in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_one :list, dependent: :destroy
-  has_many :products,throught: :list
+  has_many :products, through: :list
   has_many :purchases, through: :products
 
   after_create :give_user_a_list
