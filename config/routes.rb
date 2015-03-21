77a6033101c7d7d88e0679ed011769fc7fb1abac
katherine <muedasnka@gmail.com>
2015-03-20 20:41:43 -0600

    set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,10 @@
 Rails.application.routes.draw do
-  get 'purchases/index'
-
-  get 'purchases/show'
-
-  get 'products/index'
-
-  get 'products/new'
-
-  get 'products/createshow'
-
-  get 'products/edit'
-
-  get 'products/update'
-
-  get 'products/destroy'
-
-  root to: 'visitors#index'
   devise_for :users
   resources :users
+
+  resources :products
+  resources :purchases
+
+  root to: 'products#index'
+  
 end
